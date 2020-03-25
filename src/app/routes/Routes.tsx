ba1fa45fb59e73c097c4cc5ca8ce67859472f900
atakansavas <atakansavas@atakans-MacBook-Pro.local>
2020-03-25 20:18:54 +0300

    route fix try.

--- src/app/routes/Routes.tsx
+++ src/app/routes/Routes.tsx
@@ -12,7 +12,7 @@ export default () => {
 
     return (
         <div >
-            <Route path="/dashboard" component={Home} />
+            <Route path="/" component={Home} />
             <Route path="/companies" component={Company} />
 
         </div>
